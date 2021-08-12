1e92bebf4e15b4a50f552fb9fbc8d568282d130a
Dang Khoa <tdkhoa1101@gmail.com>
2021-08-12 21:57:43 +0700

    moi update

--- script/script.js
+++ script/script.js
@@ -7,4 +7,15 @@ window.addEventListener('scroll', function (e) {
         header.classList.remove('scroll');
         header.classList.add('fixed');
     }
-});
+})
+
+var bar = document.querySelector('#wrapper header .container p i');
+
+bar.addEventListener('click', function() {
+    var menu = document.querySelector('#wrapper header .inner-header #main-menu')
+    if(menu.style.right == "-500px") {
+        menu.style.right = "0px";
+    }else {
+        menu.style.right = "-500px";
+    }
+})
